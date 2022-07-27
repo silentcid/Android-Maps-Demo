97026e7392606cfd5adb62768b7c5ce131d8b98f
Josh Gebbeken <josh.gebbeken@bottlerocketstudios.com>
2022-07-27 13:57:14 -0500

    develop - Further cleanup of Gradle files

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -34,11 +34,32 @@ android {
 }
 
 dependencies {
+    implementation(project(mapOf("path" to ":domain")))
+    implementation("androidx.navigation:navigation-runtime-ktx:2.4.2")
+
+    // Koin - Dependency Injection
+    koinDependencies()
+
+    // LaunchPad - Starting Assets
+    launchPadDependencies()
+
+    // Accompanist
+    accompanistDependencies()
+
+    // AndroidX
+    composeDependencies()
+
+    // Android Maps Compose
     composeMapsDependencies()
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // Coil
+    coilDependencies()
+
+    // Utility
+    timberDependencies()
+
+    // Test
+    junitDependencies()
+    mockitoKotlinDependencies()
+    truthDependencies()
 }
